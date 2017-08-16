af3b5430aa40742699323a5817c0a84946b91e30
Michael Pereira <michael.fsp@gmail.com>
2017-08-16 19:39:08 +0100

    cmfe_update_BG.m 'workaround fix' to make it not crash on data containing pixels with zero variance (stuck pixels)

--- cnmfe_scripts/cnmfe_update_BG.m
+++ cnmfe_scripts/cnmfe_update_BG.m
@@ -1,9 +1,20 @@
 clear Ysignal;
 tic; 
 Ybg = Y-neuron.A*neuron.C;
+
+% add some jitter to pixels with zero variance, if necessary
+Yvar = var(Ybg,1,2);
+idx_zerovar = find(Yvar==0);
+
+if ~isempty(idx_zerovar)
+    for i=1:size(idx_zerovar,1)
+        Ybg(idx_zerovar(i),:) = Ybg(idx_zerovar(i),:) + transp(mvnrnd(0,1e-6,size(Ybg,2)));
+    end
+end
+
 rr = ceil(neuron.options.gSiz * bg_neuron_ratio); 
 active_px = []; %(sum(IND, 2)>0);  %If some missing neurons are not covered by active_px, use [] to replace IND
-[Ybg, Ybg_weights] = neuron.localBG(Ybg, spatial_ds_factor, rr, active_px, neuron.P.sn, thresh); % estiamte local background.
+[Ybg, Ybg_weights] = neuron.localBG(Ybg, spatial_ds_factor, rr, active_px, neuron.P.sn, thresh); % estimate local background.
 % subtract the background from the raw data.
 Ysignal = Y - Ybg;
 
